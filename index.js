445dfe93d6a5aa72cf2495db978c587841a588f2
Roy Shmeuli <roysh9@gmail.com>
2018-04-09 09:03:36 +0300

    Fix format

--- index.js
+++ index.js
@@ -1,63 +1,62 @@
-const LOG_LOVELS = require('./log_levels');
+'use strict'
 
-function Logger(moduleName) {
+const LOG_LEVELS = require('./log_levels')
 
-    if (!(this instanceof Logger)) {
-        return new Logger(moduleName);
-    }
+function Logger (moduleName) {
+  if (!(this instanceof Logger)) {
+    return new Logger(moduleName)
+  }
 
-    this.moduleName = moduleName;
-    this.LOG_LEVELS = LOG_LOVELS;
-    this.setLogLevel(LOG_LOVELS.DEBUG);
+  this.moduleName = moduleName
+  this.LOG_LEVELS = LOG_LEVELS
+  this.setLogLevel(LOG_LEVELS.DEBUG)
 }
 
 Logger.prototype.setLogLevel = function (logLevel) {
-    this.logLevel = logLevel;
-};
+  this.logLevel = logLevel
+}
 
 Logger.prototype.log = function (level, correlationId, message) {
-    if (level < this.logLevel) {
-        return;
-    }
+  if (level < this.logLevel) {
+    return
+  }
 
-    let formattedLog = this.formatLog(level, this.moduleName, new Date(Date.now()), correlationId, message);
-    console.log(JSON.stringify(formattedLog) + '\n');
-};
+  const formattedLog = this.formatLog(level, this.moduleName, new Date(Date.now()), correlationId, message)
+  console.log(JSON.stringify(formattedLog) + '\n')
+}
 
 Logger.prototype.formatLog = function (level, moduleName, timestamp, correlationId, message) {
+  if (!(timestamp instanceof Date)) {
+    throw new Error('timestamp must be a date')
+  }
 
-    if (!(timestamp instanceof Date)) {
-        throw "timestamp must be a date";
-    }
-
-    return {
-        "level": LOG_LOVELS.LOG_LEVELS_TRANSLATION[level],
-        "module": moduleName,
-        "timestamp": timestamp.toISOString(),
-        "correlationId": correlationId,
-        "message": message
-    }
-};
+  return {
+    'level': LOG_LEVELS.LOG_LEVELS_TRANSLATION[level],
+    'module': moduleName,
+    'timestamp': timestamp.toISOString(),
+    'correlationId': correlationId,
+    'message': message
+  }
+}
 
 Logger.prototype.debug = function (correlationId, message) {
-    this.log(LOG_LOVELS.DEBUG, correlationId, message);
-};
+  this.log(LOG_LEVELS.DEBUG, correlationId, message)
+}
 
 Logger.prototype.info = function (correlationId, message) {
-    this.log(LOG_LOVELS.INFO, correlationId, message);
-};
+  this.log(LOG_LEVELS.INFO, correlationId, message)
+}
 
 Logger.prototype.error = function (correlationId, message) {
-    this.log(LOG_LOVELS.ERROR, correlationId, message);
-};
+  this.log(LOG_LEVELS.ERROR, correlationId, message)
+}
 
 Logger.prototype.warn = function (correlationId, message) {
-    this.log(LOG_LOVELS.WARN, correlationId, message)
-};
+  this.log(LOG_LEVELS.WARN, correlationId, message)
+}
 
 Logger.prototype.fatal = function (correlationId, message) {
-    this.log(LOG_LOVELS.FATAL, correlationId, message);
-};
+  this.log(LOG_LEVELS.FATAL, correlationId, message)
+}
 
-
-module.exports = Logger;
+module.exports = Logger
